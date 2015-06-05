6c9f7f4c480e39c6385d94ffd3c44322f53d22f0
Dominik Marszk <dmarszk@gmail.com>
2015-06-05 15:37:08 +0200

    Added support for backslash syntax.

--- convert.py
+++ convert.py
@@ -66,46 +66,64 @@ def handleExistingData(iIndent):
 	sEVar = None
 	sEData = ""
 
-def dataFromString(sString, iStart = 0):
+def dataFromString(sString, sCommentOpen, iStart = 0):
 	"""
 	@brief Parse data out of a line which may or may not end in an '*/'.
 	@param sString The string to parse.
+	@param sCommentOpen What was opening tag for the comment.
 	@param iStart The starting index to parse from.  Default = 0 which is the start of the string.
 	@return The data (without the ending '*/' is present.
 	"""
 	iEnd = len(sString)
-	if CLOSE_COMMENT in sString:
+	if sCommentOpen == "/*" and CLOSE_COMMENT in sString:
 		iEnd = sString.find(CLOSE_COMMENT)
 	return sString[iStart : iEnd].rstrip()
 
-def dataFromLine(sLine):
+def dataFromLine(sLine, sCommentOpen):
 	"""
 	@brief Parse data from a comment line.
 	@param sLine The comment line to parse.
+	@param sCommentOpen What was opening tag for the comment.
 	@return A rstrip'ed string of data after the '* ' in a comment line.
 	"""
-	iStart = sLine.find("* ")
-	if iStart < 0:
-		return ""
-	iStart += 2
-	return dataFromString(sLine, iStart)
+	if sCommentOpen == "/*":
+		iStart = sLine.find("* ")
+		if iStart < 0:
+			return ""
+		iStart += 2
+	elif sCommentOpen == "///":		
+		iStart = sLine.find("/// ")
+		if iStart < 0:
+			return ""
+		iStart += 4
+	else:
+		print("dataFromLine - unknown comment opening: \"" + sCommentOpen + "\"")
+		sys.exit()
+	return dataFromString(sLine, sCommentOpen, iStart)
 		
-def handleCommentLine(sLine, iLine):
+def handleCommentLine(sLine, iLine, sCommentOpen):
 	"""
 	@brief Write data from a comment line back to the thingy.
 	@param sLine The line data.
 	@param iLine The line number.
+	@param sCommentOpen What was opening tag for the comment.
 	@return Is the end of the comment block on this line.
 	"""
 	global sEType, sEVar, sEData, iIndent
 
+	if (sCommentOpen == "///" and not sLine.strip().startswith("///")):		
+		handleExistingData(iIndent)
+		endComment()
+		# write the line as-is
+		pOutFile.write(sLine)
+		return False
 	# Work out the indentation level to operate at.
 	# This is only done once for each comment block.
 	if iIndent < 0:
-		iIndent = (len(sLine) - len(sLine.lstrip())) / 4
+		iIndent = int((len(sLine) - len(sLine.lstrip())) / 4)
 
 	# If there is no '@' symbol, save as much data as we can from the commentline.
-	if START_SYMBOL not in sLine:
+	if re.search(START_SYMBOL, sLine) is None:
 
 		# If we are a directive which only accepts single line values then anything extra is a remark.
 		if sEType in (PARAM, RETURN, AUTHOR, DATE):
@@ -114,7 +132,7 @@ def handleCommentLine(sLine, iLine):
 			sEData = ""
 
 		# Get the data from the line and append it if it is exists.
-		sData = dataFromLine(sLine)
+		sData = dataFromLine(sLine, sCommentOpen)
 		if len(sData) > 0:
 			# If we already have data, insert a breakline.
 			if sEData:
@@ -125,7 +143,7 @@ def handleCommentLine(sLine, iLine):
 				sEData = sData
 		
 		# If we have an end comment on this line, exit the comment by returning false.
-		if CLOSE_COMMENT in sLine:
+		if (sCommentOpen == "/*" and CLOSE_COMMENT in sLine):
 			handleExistingData(iIndent)
 			endComment()
 			return False
@@ -135,29 +153,30 @@ def handleCommentLine(sLine, iLine):
 	handleExistingData(iIndent)
 
 	# If this line contains an '@' symbol then work out what is after it.
-	sEType = sLine.split(START_SYMBOL)[1].split(" ")[0]
+	sEType = re.split(START_SYMBOL, sLine)[2].split()[0]
+	
 
 	# If the comment data type is BRIEF
 	if sEType == BRIEF:
-		sEData = dataFromString(sLine, sLine.find(BRIEF) + len(BRIEF) + 1)
+		sEData = dataFromString(sLine, sCommentOpen, sLine.find(BRIEF) + len(BRIEF) + 1)
 
 	elif sEType == PARAM:
-		sTemp = dataFromString(sLine, sLine.find(PARAM) + len(PARAM) + 1)
-		iChop  = sTemp.find(" ") + 1
+		sTemp = dataFromString(sLine, sCommentOpen, sLine.find(PARAM) + len(PARAM) + 1)
+		spaceMatch = re.search(r"\s+", sTemp)
+		iChop  = spaceMatch.span()[1]
 		sEData = sTemp[iChop:]
 		sEVar  = sTemp[:iChop].rstrip()
 
 	elif sEType == RETURN:
-		sEData = dataFromString(sLine, sLine.find(RETURN) + len(RETURN) + 1)
+		sEData = dataFromString(sLine, sCommentOpen, sLine.find(RETURN) + len(RETURN) + 1)
 
 	elif sEType == DATE:
-		sEData = dataFromString(sLine, sLine.find(DATE) + len(DATE) + 1)
+		sEData = dataFromString(sLine, sCommentOpen, sLine.find(DATE) + len(DATE) + 1)
 
 	elif sEType == AUTHOR:
-		sEData = dataFromString(sLine, sLine.find(AUTHOR) + len(AUTHOR) + 1)
-
+		sEData = dataFromString(sLine, sCommentOpen, sLine.find(AUTHOR) + len(AUTHOR) + 1)
 	# If we have an end comment on this line, exit the comment by returning false.
-	if CLOSE_COMMENT in sLine:
+	if sCommentOpen == "/*" and CLOSE_COMMENT in sLine:
 		handleExistingData(iIndent)
 		endComment()
 		return False
@@ -167,10 +186,13 @@ def handleCommentLine(sLine, iLine):
 import time
 import shutil
 import os
+import re
 
 ## Constants
-START_SYMBOL  = "@"
-OPEN_COMMENT  = "/**"
+## Look for @/n or \
+START_SYMBOL  = r"(@|\\)"
+## Look for /* or ///
+OPEN_COMMENT  = r"(/\*\*|///)"
 CLOSE_COMMENT = "*/"
 BRIEF         = "brief"
 PARAM         = "param"
@@ -206,30 +228,28 @@ def convert(sInFile, sOutFile = None, bReport = True):
 	iLine = 0
 	iComments = 0
 	iStartTime = time.clock()
+	sCommentOpen = ""
 
 	# Open the files.
 	pOutFile = open(sOutFile, "w")
-	with open(sInFile) as pIn:
-								  
+	with open(sInFile) as pIn:								  
 		# For each line in the file.
 		for sLine in pIn:
-
 			# Increment counter.
-			iLine += 1
-								   
+			iLine += 1								   
 			# If we are in a comment, handle the line.
 			if bInComment:
-				bInComment = handleCommentLine(sLine, iLine)
-
-			# Check the new line to see if it opens a comment line.
-			elif OPEN_COMMENT in sLine:
-				iComments += 1
-				bInComment = handleCommentLine(sLine, iLine)
-
-			# We are neither a comment so write the line back to the source.
+				bInComment = handleCommentLine(sLine, iLine, sCommentOpen)
 			else:
-				pOutFile.write(sLine)
-
+				# Check the new line to see if it opens a comment line.
+				openMatch = re.search(OPEN_COMMENT, sLine)
+				if openMatch:
+					sCommentOpen = openMatch.group(0)
+					iComments += 1
+					bInComment = handleCommentLine(sLine, iLine, sCommentOpen)
+				# We are neither a comment so write the line back to the source.
+				else:
+					pOutFile.write(sLine)
 	# Close the output file.
 	pOutFile.close()
 	
@@ -237,23 +257,23 @@ def convert(sInFile, sOutFile = None, bReport = True):
 	#shutil.copy(sInFile, sInFile + "_dbackup")
 	
 	# Copy the new file over the old file.
-	shutil.copy(sOutFile, sInFile)
+	#shutil.copy(sOutFile, sInFile)
 	
-	os.remove(sOutFile)
+	#os.remove(sOutFile)
 	
 	# Report.
 	if bReport:
-		print sInFile
-		print str(iComments) + " comment blocks converted within "+str(iLine)+" lines in approx "+str(round(time.clock() - iStartTime, 2))+" seconds."
+		print (sInFile)
+		print (str(iComments) + " comment blocks converted within "+str(iLine)+" lines in approx "+str(round(time.clock() - iStartTime, 2))+" seconds.")
 
 
 if __name__ == "__main__":
 	import sys
 	if len(sys.argv) == 1:
-		print "Please specify an input file."
+		print ("Please specify an input file.")
 	else:
 		lFiles = sys.argv[1:]
 		for sFile in lFiles:
 			convert(sFile)
-			print "-----"
-		raw_input("Done")
+			print ("-----")
+		#input("Done")
